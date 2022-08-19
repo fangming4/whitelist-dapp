1a0bb5649b227576c24606510c473031ca3829af
fangming4 <fangming4@outlook.com>
2022-08-19 20:12:10 +0800

    init project

---
+++ hardhat-tutorial/hardhat.config.ts
@@ -0,0 +1,20 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env" });
+
+const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
+const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.9",
+  networks: {
+    rinkeby: {
+      url: ALCHEMY_API_KEY_URL,
+      accounts: [RINKEBY_PRIVATE_KEY || ""],
+    },
+  },
+};
+
+export default config;
